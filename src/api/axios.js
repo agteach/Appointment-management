bc80c9b8330607a3e710dc4a8e8f372c58c4471b
Ag teach <agteach5@gmail.com>
2026-05-09 00:02:07 +0300

    production api url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 import useAuthStore from "../store/authStore";
 
-const baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
-
-const instance = axios.create({
-    baseURL,
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
-instance.interceptors.request.use((config) => {
+api.interceptors.request.use((config) => {
     const token = useAuthStore.getState().token;
 
     if (token) {
@@ -17,4 +15,4 @@ instance.interceptors.request.use((config) => {
     return config;
 });
 
-export default instance;
+export default api;
